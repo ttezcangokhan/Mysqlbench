6f04cfdcbbeda40925a539a600745bedef6979e6
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-01-28 23:58:29 +0100

    update010101

---
+++ IdeaProjects/com.TestNGBatch44/src/test/java/tests/day11/C03_TestBaseIlkTest.java
@@ -0,0 +1,18 @@
+package tests.day11;
+
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_TestBaseIlkTest extends TestBase {
+
+
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+
+
+    }
+
+
+}
