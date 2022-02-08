ea01ef71a6adc34752fbeea4e04594b76216e60e
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-02-08 22:00:32 +0100

    0802202

---
+++ IdeaProjects/com.Batch44POM/src/test/java/pages/FacebookPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class FacebookPage {
+    public FacebookPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//button[@title='Allow All Cookies']")
+    public WebElement facebookCookies;
+
+
+    @FindBy(name="email")
+    public WebElement emailKutusu;
+
+    @FindBy(id="pass")
+    public WebElement passKutusu;
+
+    @FindBy(name="login")
+    public WebElement loginButonu;
+
+    @FindBy(className = "_9ay7")
+    public WebElement girilemediYazisiElemnti;
+
+
+
+}
+
---
+++ IdeaProjects/com.Batch44POM/src/test/java/utilities/Driver.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+ static  WebDriver driver;
+
+    public static WebDriver getDriver(){
+        WebDriverManager.chromedriver().setup();
+        if (driver == null){
+            driver=new ChromeDriver();
+        }
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+
+
+    }
+    public static void cloaserDriver(){
+        driver.quit();
+    }
+
+    public static void closeDriver() {
+    }
+}
