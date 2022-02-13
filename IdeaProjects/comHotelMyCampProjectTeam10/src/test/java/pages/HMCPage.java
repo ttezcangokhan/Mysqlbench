fe737cb764a5b443f934597ac6b85ad694a15ad9
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-02-13 09:38:46 +0100

    ver..

---
+++ IdeaProjects/comHotelMyCampProjectTeam10/src/test/java/pages/HMCPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HMCPage {
+
+    public HMCPage {
+        public HMCPage() {
+            PageFactory.initElements(Driver.getDriver(), this);
+        }
+
+        //Yönetici Giris yap metodu olusturulacak
+        //
+    }
+}
