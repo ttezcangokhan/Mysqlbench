bebbe956e5f1149c5bba86749d6a33456b2b7c81
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-03-14 23:59:45 +0100

    update

--- IdeaProjects/batch44Api/src/test/java/test_data/DummyTestData.java
+++ IdeaProjects/batch44Api/src/test/java/test_data/DummyTestData.java
@@ -1,5 +1,7 @@
 package test_data;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,12 +28,22 @@ public class DummyTestData {
    */
 
     //40,21 ve 19 yaslarında çalışanlar olup olmadığını
-    public HashMap<String, Object> setUpTestData(){
+    public HashMap<String, Object> setUpTestData() {
         List<Integer> yaslar = new ArrayList<>();
         yaslar.add(40);
         yaslar.add(21);
         yaslar.add(19);
 
+
+        /*
+           {
+           "id": 10,
+           "employee_name": "Sonya Frost",
+           "employee_salary": 103600,
+           "employee_age": 23,
+           "profile_image": ""
+    }
+         */
         HashMap<String, Object> onuncu = new HashMap<>();
         onuncu.put("id", 10);
         onuncu.put("employee_name", "Sonya Frost");
@@ -42,16 +54,15 @@ public class DummyTestData {
 
         /*
     Status kodun 200 olduğunu,
-   14. Çalışan isminin "Haley Kennedy" olduğunu,
-   Çalışan sayısının 24 olduğunu,
-   Sondan 3. çalışanın maaşının 675000 olduğunu
-   40,21 ve 19 yaslarında çalışanlar olup olmadığını
-   10. Çalışan bilgilerinin bilgilerinin aşağıdaki gibi
+    14. Çalışan isminin "Haley Kennedy" olduğunu,
+    Çalışan sayısının 24 olduğunu,
+    Sondan 3. çalışanın maaşının 675000 olduğunu
+    40,21 ve 19 yaslarında çalışanlar olup olmadığını
+    10. Çalışan bilgilerinin bilgilerinin aşağıdaki gibi
          */
-
         HashMap<String, Object> expectedData = new HashMap<>();
         expectedData.put("statusCode", 200);
-        expectedData.put("ondurduncucalisan", "Haley Kennedy");
+        expectedData.put("ondorduncucalisan", "Haley Kennedy");
         expectedData.put("calisansayisi", 24);
         expectedData.put("sondanucuncucalisaninmaasi", 675000);
         expectedData.put("arananyaslar", yaslar);
@@ -59,15 +70,59 @@ public class DummyTestData {
         return expectedData;
     }
 
-  /*   {"name":"Ali Can",
-            "salary":"2000",
-            "age":"40",}*/
-  public HashMap<String, Object> setUpRequestBody() {
 
-      HashMap<String, Object> requestBody = new HashMap<>();
-      requestBody.put("name", "Ali Can");
-      requestBody.put("salary", "2000");
-      requestBody.put("age", "40");
-      return requestBody;
+
+    /*
+       {
+       "name":"Ali Can",
+       "salary":"2000",
+       "age":"40",
+   }
+     */
+    public HashMap<String, Object> setUpRequestBody() {
+
+        HashMap<String, Object> requestBody = new HashMap<>();
+        requestBody.put("name", "Ali Can");
+        requestBody.put("salary", "2000");
+        requestBody.put("age", "40");
+        return requestBody;
+    }
+
+    /*
+       gönderildiğinde,Status kodun 200 olduğunu ve dönen response body nin,
+   {
+      "status": "success",
+       "data": {
+       “id”:…
+   },
+       "message": "Successfully! Record has been added."
+   }
+     */
+
+    public HashMap<String, Object> setUpExpectedData() {
+
+        HashMap<String, Object> expectedData = new HashMap<>();
+        expectedData.put("statusCode", 200);
+        expectedData.put("status", "success");
+        expectedData.put("message", "Successfully! Record has been added.");
+        return expectedData;
+    }
+
+    /*
+    http://dummy.restapiexample.com/api/v1/delete/2 bir DELETE request gönderdiğimde
+
+    Dönen response un status kodunun 200 ve body kısmının aşağıdaki gibi olduğunu test edin
+    {
+    "status": "success",
+    "data": "2",
+    "message": "Successfully! Record has been deleted"
+    }
+   */
+    public JSONObject setUpDeleteExpectedData(){
+        JSONObject expectedData = new JSONObject();
+        expectedData.put("status", "success");
+        expectedData.put("data", "2");
+        expectedData.put("message", "Successfully! Record has been deleted");
+        return expectedData;
     }
 }
