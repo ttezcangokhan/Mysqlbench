9f4fab255a7dfbe50418e338c2005d7689dbec56
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-05-11 23:13:32 +0200

    update110522

---
+++ IdeaProjects/SnakeProje/src/main/java/zetcode/Snake4.java
@@ -0,0 +1,32 @@
+package zetcode;
+
+public class Snake4 {
+
+    static int d ;
+    public static void main(String[] args){
+
+        int a;
+
+        a =10;
+
+        //int b = 20;
+        //int c;
+        //System.out.println(d);
+        //int x=10,y=20;
+
+        System.out.println(a);
+        a=30;
+        System.out.println(a);
+        final int c = 20;
+        System.out.println(c);
+        c=30;
+        System.out.println(c);
+
+
+
+
+
+    }
+
+}
+
