9e3b7354704610639aca1cb8c1cab5fe84089d47
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-05-20 10:45:20 +0200

    update200522

---
+++ IdeaProjects/com.seleniumilkProje/src/test/java/day02_driverMethods/C01_Tekrar.java
@@ -0,0 +1,51 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_Tekrar {
+    // Normalde slenium'un kendi IDE'si de var fakat biz daha kullanisli oldugu icin intelliJ kullaniyoruz
+    // intellij'de yeni bir proje actigimizda oncelikle selenium kutuphanelerini projeye eklememiz gerekir
+    // biz en ilkel haliyle projemize kutuphaneleri jar dosyalari olarak yukledik
+    // bu ekledigimiz dosyalarla artik driver'in ayarlarini yapabiliriz
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        // setProperty method'u 2 parametre ister
+        // ilki kullanacagimiz browser'a ait driver
+        // ikinci parametre ise selenium itesinden indirip projemize ekledigimiz chromedriver'in path'i
+        // windows kullanicilari sona .exe eklerken, apple kullananlar .exe eklememeli
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+
+        Thread.sleep(5000);
+        // java'dan gelir, icine yazilan milisaniye kadar kodlarin calismasini durdurur
+
+        driver.close();
+        // driver.colose class'in sonuna yazilir. Cunku bu kod calisinca driver'imiz kapanir
+        // bu koddan sonra yeniden bir browser acmak istiyorsak driver'a yeni deger atamaliyiz
+
+
+
+
+    }
+
+    public static class C02_DriverGetMethodlari {
+
+        public static void main(String[] args) {
+            System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+             WebDriver driver= new ChromeDriver();
+
+            driver.get("https://www.amazon.com");
+
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+            System.out.println(driver.getWindowHandle());
+            System.out.println(driver.getPageSource());
+
+        }
+    }
+}
---
+++ IdeaProjects/javafunctional01/src/main/java/day02_driverMethods/C01_Tekrar.java
@@ -0,0 +1,7 @@
+package day02_driverMethods;
+
+public class C01_Tekrar {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","")
+    }
+}
