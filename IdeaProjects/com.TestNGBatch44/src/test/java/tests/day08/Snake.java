f6ded526b778220ace5fff2305534a194f6c35fb
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-05-28 20:43:45 +0200

    update280522

---
+++ IdeaProjects/com.TestNGBatch44/src/test/java/tests/day08/Snake.java
@@ -0,0 +1,34 @@
+package tests.day08;
+
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+    public class Snake extends JFrame {
+
+        public Snake() {
+
+            initUI();
+        }
+
+        private void initUI() {
+
+            add(new day08.Board());
+
+            setResizable(false);
+            pack();
+
+            setTitle("Deniz Kayranin Snegi");
+            setLocationRelativeTo(null);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+
+        public static void main(String[] args) {
+
+            EventQueue.invokeLater(() -> {
+                JFrame ex = new Snake();
+                ex.setVisible(true);
+            });
+        }
+    }
+
