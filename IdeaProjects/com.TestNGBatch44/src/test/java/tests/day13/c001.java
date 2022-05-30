848d19eb22bc26036ac459902c0eb5ad0ee739b4
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-05-30 23:49:42 +0200

    update300522

--- IdeaProjects/com.TestNGBatch44/src/test/java/tests/day13/c001.java
+++ IdeaProjects/com.TestNGBatch44/src/test/java/tests/day13/c001.java
@@ -1,35 +1,34 @@
 package tests.day13;
 
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class c001 extends TestBase {
+
     @Test
 
     public void test01(){
-        driver.get("https://html.com/tags/iframe/");
-
-        Actions actions=new Actions(driver);
-         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.ARROW_DOWN).perform();
-        WebElement iframeElementi=driver.findElement(By.xpath("//iframe[@src='https://www.youtube.com/embed/owsfdh4gxyc']"));
-        driver.switchTo().frame(iframeElementi);
-        WebElement fullscreenButton=driver.findElement(By.xpath("//button[@class='ytp-fullscreen-button ytp-button']"));
-        actions.click(fullscreenButton).perform();
+        String dosyaYolu=System.getProperty("user.home")+"\\downloads\\download.png";
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
 
 
 
 
+    }
 
+    @Test
 
-        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
+    public void downloadTesti() throws InterruptedException {
 
-
-
+        driver.get("https://the-internet.herokuapp.com/download");
+        driver.findElement(By.xpath("//a[.='download.png']")).click();
+        Thread.sleep(5000);
     }
 
 }
