cbaac4f908ba1ab83cc28035ca209af6284d21dc
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-06-04 23:52:18 +0200

    update0040622

--- IdeaProjects/com.TestNGBatch44/src/test/java/tests/day14/Calisma.java
+++ IdeaProjects/com.TestNGBatch44/src/test/java/tests/day14/Calisma.java
@@ -38,6 +38,26 @@ public class Calisma extends TestBase {
         WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15));
         wait.until(ExpectedConditions.)
 
+                package tests.day13;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+        public class C07_ExplicitlyWait extends TestBase {
+
+            @Test
+            public void test01(){
+
+
+                WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(50));
+                //wait.until(ExpectedConditions.)
+            }
+        }
+
 
     }
 
